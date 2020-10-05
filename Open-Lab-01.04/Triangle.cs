9c75ffcfa5273f3e99f60c67b1243c47edf63926
Jakub Dipčár <jakub.dipcar@gmail.com>
2020-10-05 11:57:05 +0200

    done

--- Open-Lab-01.04/Triangle.cs
+++ Open-Lab-01.04/Triangle.cs
@@ -6,7 +6,9 @@ namespace Open_Lab_01._04
     {
         public double Area(double foundation, double height)
         {
-            throw new NotImplementedException();
+            double halfoundation = foundation / 2;
+            double triangle = halfoundation * height;
+            return triangle;
         }
     }
 }
